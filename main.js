537a7722d4690cd19565ff64e372c6e5683e77c0
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-04-28 16:15:52 -0700

    update js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("i can see it")
+document.addEventListener("DOMContentLoaded", e => e.preventDefault());
+
+const url = "https://www.bcferriesapi.ca/api/TSA/SWB/";
+fetch(url)
+  .then(response => response.json())
+  .then(photos => {
+    console.log(photos)
+  })
+  .catch(error => {
+    console.error(error);
+  })
