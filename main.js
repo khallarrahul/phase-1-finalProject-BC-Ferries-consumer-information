e3a9a13af3bc3df4700c7f35f49126f6a8224b2b
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-04-28 19:23:04 -0700

    updates css and js

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-document.addEventListener("DOMContentLoaded", e => e.preventDefault());
+
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+    e.preventDefault()})
 
 const url = "https://www.bcferriesapi.ca/api/TSA/SWB/";
+function getFerry(){
 fetch(url)
   .then(response => response.json())
-  .then(photos => {
-    console.log(photos)
+  .then(sailings => {
+    renderFerry(sailings)
   })
-  .catch(error => {
-    console.error(error);
-  })
+}
+getFerry()
+
+function renderFerry(sailings){
+    // const ferryCollection = document.querySelector("#ferryCollection");
+                sailings.sailings.forEach(sailing => {console.log(sailing)
+                    const card = document.createElement("div");
+                    card.classList.add("card");
+                    const h2 = document.createElement("h2");
+                    h2.textContent = sailing.vesselName;
+                    const p1 = document.createElement("p");
+                    p1.textContent = `Departing at ${sailing.time}`;
+                    const p2 = document.createElement("p");
+                    p2.textContent = `Percentage occupied is ${sailing.fill} %`;
+                    card.appendChild(h2);
+                    card.appendChild(p1);
+                    card.appendChild(p2);
+                    ferryCollection.appendChild(card);
+    })
+}
+
+// setInterval(() => {
+//     location.reload();
+//   }, 30000);
+
