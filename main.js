8531ab425cac519f2e5a5991e6f9215671645ecf
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-05-01 14:47:35 -0700

    init button to render ferries

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
 
+let addFerry = false;
 
-document.addEventListener("DOMContentLoaded", (e)=>{
-    e.preventDefault()})
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.querySelector(".showFerryList");
+  const ferryCollection = document.querySelector("#ferryCollection");
+  const list = document.querySelector("#showFerryList");
 
-const url = "https://www.bcferriesapi.ca/api/TSA/SWB/";
-function getFerry(){
-fetch(url)
-  .then(response => response.json())
-  .then(sailings => {
-    renderFerry(sailings)
-  })
-}
-getFerry()
+  function getFerry() {
+    fetch("https://www.bcferriesapi.ca/api/TSA/SWB/")
+      .then(response => response.json())
+      .then(sailings => {
+        renderFerry(sailings)
+      })
+  }
 
-function renderFerry(sailings){
-    // const ferryCollection = document.querySelector("#ferryCollection");
-                sailings.sailings.forEach(sailing => {console.log(sailing)
-                    const card = document.createElement("div");
-                    card.classList.add("card");
-                    const h2 = document.createElement("h2");
-                    h2.textContent = sailing.vesselName;
-                    const p1 = document.createElement("p");
-                    p1.textContent = `Departing at ${sailing.time}`;
-                    const p2 = document.createElement("p");
-                    p2.textContent = `Percentage occupied is ${sailing.fill} %`;
-                    card.appendChild(h2);
-                    card.appendChild(p1);
-                    card.appendChild(p2);
-                    ferryCollection.appendChild(card);
-    })
-}
+  function renderFerry(sailings) {
+    sailings.sailings.forEach(sailing => {
+      console.log(sailing)
+      const card = document.createElement("div");
+      card.classList.add("card");
+      const h2 = document.createElement("h2");
+      h2.textContent = sailing.vesselName;
+      const p1 = document.createElement("p");
+      p1.textContent = `Departing at ${sailing.time}`;
+      const p2 = document.createElement("p");
+      p2.textContent = `Percentage occupied is ${sailing.fill} %`;
+      const btn = document.createElement("button");
+      btn.textContent = "Add to calendar";
 
-// setInterval(() => {
-//     location.reload();
-//   }, 30000);
+      card.appendChild(h2);
+      card.appendChild(p1);
+      card.appendChild(p2);
+      card.append(btn);
+      ferryCollection.appendChild(card);
+    });
+  }
+});
 
